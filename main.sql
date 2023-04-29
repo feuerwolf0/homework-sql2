3feba161041f61027c4a1f9c1a8fe0cb8a4f1a32
Poletaev Kirill <poletaev777@gmail.com>
2023-04-29 22:51:20 +0300

    Задание выполнено

---
+++ main.sql
@@ -0,0 +1,54 @@
+-- таблица всех артистов
+CREATE TABLE IF NOT EXISTS artists (
+artist_id SERIAL PRIMARY KEY,
+name VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- таблица всех жанров
+CREATE TABLE IF NOT EXISTS genres (
+genre_id SERIAL PRIMARY KEY,
+genre VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- промежуточная таблица жанр-артист
+CREATE TABLE IF NOT EXISTS genreArtist (
+artist_id INTEGER REFERENCES artists(artist_id),
+genre_id INTEGER REFERENCES genres(genre_id)
+);
+
+-- таблица всех албомов
+CREATE TABLE IF NOT EXISTS albums (
+album_id SERIAL PRIMARY KEY,
+title VARCHAR(255) NOT NULL,
+year DATE NOT NULL
+);
+
+--промежуточная таблица альбом-артист
+CREATE TABLE IF NOT EXISTS albumArtist (
+album_id INTEGER REFERENCES albums(album_id),
+artist_id INTEGER REFERENCES artists(artist_id),
+CONSTRAINT unique_album_id UNIQUE (album_id)
+);
+
+--таблица всех треков
+CREATE TABLE IF NOT EXISTS tracks (
+track_id SERIAL PRIMARY KEY,
+title VARCHAR(255) NOT NULL,
+duration INTEGER NOT NULL,
+album_id INTEGER REFERENCES albums(album_id)
+);
+
+-- таблица всех сборников
+CREATE TABLE IF NOT EXISTS collections (
+collection_id SERIAL PRIMARY KEY,
+title VARCHAR(255) NOT NULL,
+year DATE NOT NULL
+);
+
+--промежуточная таблица сборник-трек
+CREATE TABLE IF NOT EXISTS collectionTrack (
+track_id INTEGER REFERENCES tracks(track_id),
+collection_id INTEGER REFERENCES collections(collection_id)
+);
+
+
